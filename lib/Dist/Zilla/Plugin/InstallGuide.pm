e74ce0ae0925bc1049e306ffe065512046951d5a
Marcel Gruenauer <hanekomu@gmail.com>
2010-05-26 10:08:15 +0200

    added support for Module::Build; note cpanm as preferred installation method

--- lib/Dist/Zilla/Plugin/InstallGuide.pm
+++ lib/Dist/Zilla/Plugin/InstallGuide.pm
@@ -7,32 +7,30 @@ package Dist::Zilla::Plugin::InstallGuide;
 # ABSTRACT: Build an INSTALL file
 use Moose;
 use Moose::Autobox;
-with 'Dist::Zilla::Role::FileGatherer';
+with 'Dist::Zilla::Role::InstallTool';
 with 'Dist::Zilla::Role::TextTemplate';
 
-sub gather_files {
-    my $self = shift;
-    require Dist::Zilla::File::InMemory;
-    (my $main_package = $self->zilla->name) =~ s!-!::!g;
-    my $template = q|
+my $template = q|
 This is the Perl distribution {{ $dist->name }}.
 
 ## Installation
 
 {{ $dist->name }} installation is straightforward.
-If your CPAN shell is set up, you should just be able to do
+
+### Installation with cpanm
+
+If you have cpanm, you only need one line:
+
+    % cpanm {{ $package }}
+
+### Installating with the CPAN shell
+
+Alternatively, if your CPAN shell is set up, you should just be able to do:
 
     % cpan {{ $package }}
 
-Download it, unpack it, then build it as per the usual:
-
-    % perl Makefile.PL
-    % make && make test
-
-Then install it:
-
-    % make install
-
+### Manual installation
+{{ $manual_installation }}
 ## Documentation
 
 {{ $dist->name }} documentation is available as in POD.
@@ -42,10 +40,53 @@ So you can do:
 
 to read the documentation with your favorite pager.
 |;
+
+my $makemaker_manual_installation = q|
+As a last resort, you can manually install it. Download it, unpack it, then
+build it as per the usual:
+
+    % perl Makefile.PL
+    % make && make test
+
+Then install it:
+
+    % make install
+|;
+
+my $module_build_manual_installation = q|
+As a last resort, you can manually install it. Download it, unpack it, then
+build it:
+
+    % perl Build.PL
+    % ./Build && ./Build test
+
+Then install it:
+
+    % ./Build install
+|;
+
+sub setup_installer {
+    my $self = shift;
+    my $manual_installation;
+    for (@{ $self->zilla->files }) {
+        if ($_->name eq 'Makefile.PL') {
+            $manual_installation = $makemaker_manual_installation;
+            last;
+        } elsif ($_->name eq 'Build.PL') {
+            $manual_installation = $module_build_manual_installation;
+            last;
+        }
+    }
+    unless (defined $manual_installation) {
+        $self->log_fatal('neither Makefile.PL nor Build.PL is present, aborting');
+    }
+    require Dist::Zilla::File::InMemory;
+    (my $main_package = $self->zilla->name) =~ s!-!::!g;
     my $content = $self->fill_in_string(
         $template,
-        {   dist    => \($self->zilla),
-            package => $main_package
+        {   dist                => \($self->zilla),
+            package             => $main_package,
+            manual_installation => $manual_installation
         }
     );
     my $file = Dist::Zilla::File::InMemory->new(
